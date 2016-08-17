9ed994bd47f1531488dfcdc8798a11d72ba89b10
Michał Lipka <michallipka@gmail.com>
2016-08-17 18:32:07 +0200

    init

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App</h1><h2>dupa 1234 dupa</h2><h3><button>Show menu</button></h3>'
+})
+export class AppComponent { }
